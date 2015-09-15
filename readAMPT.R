c79fa070048b7d6c9c4e9f722e25d0786d221c9a
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-09-15 17:57:30 -0400

    Made deltaR table for non-identical particle pairs Added nonID results to results table

--- readAMPT.R
+++ readAMPT.R
@@ -70,7 +70,8 @@ GetDeltaRTable <- function(data) {
     xDiffs <- GetDiffsByEventForColumn(data,data$X)
     yDiffs <- GetDiffsByEventForColumn(data,data$Y)
     zDiffs <- GetDiffsByEventForColumn(data,data$Z)
-    dframe <- cbind(xDiffs,yDiffs,zDiffs)
+    deltaR <- sqrt(xDiffs^2 + yDiffs^2 + zDiffs^2)
+    dframe <- cbind(xDiffs,yDiffs,zDiffs, deltaR)
     as.data.table(dframe)
 }
 
@@ -78,17 +79,48 @@ deltaRLambdas <- GetDeltaRTable(lambdas)
 deltaRAntilambdas <- GetDeltaRTable(antilambdas)
 
 
-# Try doing it to non-identical particles.  Here, we need to mix between species
+
+# Try doing this for non-identical particles.  Here, we need to mix between species
 DiffNonIdentical <- function(vals1, vals2) {
     diffMatrix <- sapply(vals1, function(x) x - vals2)
 }
 
+# This works, but would be nice to just pass a column name in the future
+GetDiffsByEventForColumnNotID <- function(data, column, data2, data2column) {
+    splitData1 <- with(data, split(column, Event))
+    splitData2 <- with(data2, split(data2column, Event))
+    splitDataDiffs <- mapply(DiffNonIdentical, splitData1, splitData2)
+    diffsCombined <- unlist(splitDataDiffs, use.names=FALSE)
+}
 
+GetDeltaRTableNotID <- function() {
+    xDiffs <- GetDiffsByEventForColumnNotID(lambdas, lambdas$X, 
+                                           antilambdas, antilambdas$X)
+    yDiffs <- GetDiffsByEventForColumnNotID(lambdas, lambdas$Y, 
+                                           antilambdas, antilambdas$Y)
+    zDiffs <- GetDiffsByEventForColumnNotID(lambdas, lambdas$Z, 
+                                           antilambdas, antilambdas$Z)
+    deltaR <- sqrt(xDiffs^2 + yDiffs^2 + zDiffs^2)
+    dframe <- cbind(xDiffs,yDiffs,zDiffs, deltaR)
+    as.data.table(dframe)
+}
 
+deltaRNotID <- GetDeltaRTableNotID()
 
-#xDiffMixed <- DiffNonIdentical(lambdas$X,antilambdas$X)
+# Compute mean, standard deviation, and standard error
+se <- function(x) sqrt(var(x)/length(x))
 
-
-
+results <- data.frame(
+    PDG = c(3122, -3122, 0),
+    MeanDeltaR = c(mean(deltaRLambdas$deltaR), 
+                   mean(deltaRAntilambdas$deltaR),
+                   mean(deltaRNotID$deltaR)),
+    Std = c(sd(deltaRLambdas$deltaR), 
+            sd(deltaRAntilambdas$deltaR),
+            sd(deltaRNotID$deltaR)),
+    StdErr = c(se(deltaRLambdas$deltaR), 
+               se(deltaRAntilambdas$deltaR),
+               se(deltaRNotID$deltaR))              
+)
 
 
